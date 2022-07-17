5405eb7a5a9f965a9ba4bae8036789649b92137e
Olivia <oliviabennington@gmail.com>
2022-07-17 19:20:09 -0400

    location not found

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 //
 const URL = "6c837dfcef697002afd6a4d3377a8f81&q="
-const section = document.getElementById("weather")
 const form = document.querySelector("form")
+const section = document.getElementById("weather")
 const search = document.getElementById("weather-search")
 
 form.onsubmit = async (e) => {
     e.preventDefault()
+    userQuery = search.value
+    search.value = ""
+    if (!userQuery){
+        return
+    }
+  const forecast = await fetch('https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=' + URL + userQuery)
+  .catch()
+  const notation = await forecast.json()
+    if ((notation.cod && notation.cod != 200) || notation.data){
+      locationNotFound()
+      return
+    } info(notation)
+};
 
-
-const displayData = (res) => {
-weatherSection.innerHTML = ""
+const locationNotFound = () => { section.innerHTML = ""
+  errMessage = document.createElement('h2')
+  errMessage.textContent = 'Location not found. Please enter a valid city.'
+  section.replaceChildren(errMessage)
+};
 
 
 
